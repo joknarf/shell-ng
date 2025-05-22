5ecab7c2a42f402a88a585f23a3cfb8e137beb48
joknarf <joknarf@free.fr>
2025-05-22 23:22:52 +0200

    complete-ng after seede

--- shell-ng.plugin.bash
+++ shell-ng.plugin.bash
@@ -1,7 +1,7 @@
 _path_script="$(cd "${BASH_SOURCE%/*}";pwd)"
 . $_path_script/nerdp
 . $_path_script/selector
-. $_path_script/complete-ng.bash
 . $_path_script/redo
 . $_path_script/seedee
+. $_path_script/complete-ng.bash
 unset _path_script
